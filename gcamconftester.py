cc07d42df5ce03101d100cb53dcbf696dfec11d3
Sergey Rivov <rvngtn@live.com>
2021-05-26 15:06:40 +0300

    логи

--- gcamconftester.py
+++ gcamconftester.py
@@ -1,7 +1,15 @@
 from lxml import etree
-import os, sys, math
-import numpy as np
-from itertools import accumulate
+import os, sys, math, logging
+import numpy as np  
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("gcam.log"),
+        logging.StreamHandler()
+    ]
+)
 
 def get_key_from_camera_preferences(config_key):
     """
@@ -9,14 +17,17 @@ def get_key_from_camera_preferences(config_key):
     """
     try:
         tree = etree.parse("camera_preferences.xml")
+        logging.info("Нашел camera_preferences.xml")
         root = tree.getroot()
         search_string = ".//ListPreference[@android:key='" + config_key + "']"
         key_element = root.find(search_string, root.nsmap)
+        logging.info("Нашел нужный ключ - {0}".format(config_key))
     except IOError as e:
-        print("Не могу обработать camera_preferences.xml - %sn" % e)
+        logging.error("Не могу обработать camera_preferences.xml - %sn" % e)
     
     entries = key_element.attrib.get('{http://schemas.android.com/apk/res/android}entries') #лол блять
     entryValues = key_element.attrib.get('{http://schemas.android.com/apk/res/android}entryValues')
+    logging.info("Нашел массивы значений для ключа {0}: {1} и {2}".format(config_key, entries[7:], entryValues[7:]))
     return entries, entryValues
 
 def get_values_from_arrays(entries, entryValues):
@@ -25,6 +36,7 @@ def get_values_from_arrays(entries, entryValues):
     """
     try:
         tree = etree.parse("arrays.xml")
+        logging.info("Нашел arrays.xml")
         root = tree.getroot()
         entries = entries[7:]
         entryValues = entryValues[7:]
@@ -33,7 +45,7 @@ def get_values_from_arrays(entries, entryValues):
         entryValues_search_string = ".//string-array[@name='" + entryValues + "']"
         entryValues_element = root.find(entryValues_search_string)
     except IOError as e:
-        print("Не могу обработать arrays.xml - %sn" % e)
+        logging.error("Не могу обработать arrays.xml - %sn" % e)
 
     entries_array = []
     entryValues_array = []
@@ -44,7 +56,7 @@ def get_values_from_arrays(entries, entryValues):
         entryValues_array.append(element.text)
     
     entries_hash = np.array(list(zip(entries_array,entryValues_array)))
-    #print(entries_hash)
+    logging.info("Нашел список значений для ключа: от {0} ({1}) до {2} ({3})".format(entries_array[0], entryValues_array[0], entries_array[-2], entryValues_array[-2]))
     return entries_hash
 
 def get_number_of_items_from_array(entries_hash, div):
@@ -52,25 +64,40 @@ def get_number_of_items_from_array(entries_hash, div):
     Выдает нужное количество значений из массива расположенных на равном промежутке
     """
     num = len(entries_hash)
+    logging.info("Делю весь массив из {0} значений на {1} промежутков".format(num, div))
     #div = 5
     entries_hash_count = [num // div + (1 if x < num % div else 0)  for x in range (div)]
     entries_hash_count = np.cumsum(entries_hash_count)
-    print(entries_hash_count)
     entries_hash_count = np.insert(entries_hash_count, 0, 1)
-    print(entries_hash_count)
+    logging.info("Магические числа {0}".format(entries_hash_count-1))
     entries_hash = np.array(entries_hash)
     return entries_hash[entries_hash_count-1]
+
+def find_and_write_to_xml(config_name, config_key, value):
+    """
+    Ищет и пишет в хмл нужное значений в нужный ключ 
+    """
+    #<string name="lib_sharpness_key">080080D2</string>
+    try:
+        tree = etree.parse(config_name)
+        root = tree.getroot()
+        config_search_string = ".//string[@name='" + config_key + "']"
+        config_element = root.find(config_search_string)
+        #print(config_element.text)
+    except IOError as e:
+        logging.error("Не могу обработать {0} - {1}".format(config_name, e))
+
 #def save_to_xml(config_to_save):
 
 #def connect_with_adb():
 
-
-
 if __name__ == "__main__":
     config_name = "8.2riv.xml"
-    config_key = "lib_grid_key"
+    config_key = "lib_sharpness_key"
+    logging.info("Буду подбирать значения ключа {0} для конфига {1}".format(config_key, config_name))
     entries, entryValues = get_key_from_camera_preferences(config_key)
     entries_hash = get_values_from_arrays(entries, entryValues)
     entries_hash = entries_hash[:-1] #убирает Off значение из списка
-    print(get_number_of_items_from_array(entries_hash, 5))
-    
+    entries_hash = get_number_of_items_from_array(entries_hash, 5)
+    for entry in entries_hash:
+        logging.info("Обрабатываю {0} = {1}".format(entry[0], entry[1]))
