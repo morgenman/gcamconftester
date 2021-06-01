14bd2856dce454bfed0033463e96ffa12e31d34f
Sergey Rivov <rvngtn@live.com>
2021-06-01 14:14:54 +0300

    чуть фикса

--- get_conf.py
+++ get_conf.py
@@ -2,9 +2,6 @@ from gcamconftester import adb_command
 from gcamconftester import gcam_package
 import sys
 
-if len(sys.argv) > 1:
-    config = sys.argv[1]
-else:
-    config = "perrot043top.xml"
+config = sys.argv[1] if len(sys.argv) > 1 else "perrot043top.xml"
 adb_command('root')
 adb_command(f'pull /data/data/{gcam_package}/shared_prefs/{gcam_package}_preferences.xml {config}')
