8fdd3a28e51935fabf34dd9a25f71f31fd21743b
Sergey Rivov <rvngtn@live.com>
2021-06-02 15:28:36 +0300

    убрал удаление последнего значения в списке

--- gcamconftester.py
+++ gcamconftester.py
@@ -45,7 +45,7 @@ def adb_command(command):
         exit()
         response = 'Ошибка!'
     except subprocess.TimeoutExpired:
-        logging.error('Процесс adb превысил время ожидания и был закрыт. Попробуем еще раз')
+        logging.error('Процесс adb превысил время ожидания и был закрыт. Попробуем еще раз [{0}]'.format(command))
         response = adb_command(command)
     return response
 
@@ -373,7 +373,7 @@ if __name__ == "__main__":
     logging.info("Буду подбирать значения ключа {0} для конфига {1}".format(config_key, config_name))
     entries, entryValues = get_key_from_camera_preferences(config_key)
     entries_hash = get_values_from_arrays(entries, entryValues)
-    entries_hash = entries_hash[:-1] #убирает Off значение из списка
+    #entries_hash = entries_hash[:-1] #убирает Off значение из списка
     entries_hash = get_number_of_items_from_array(entries_hash, num_values_to_test)
     if args.bsg:
             config_key = config_key + "_" + args.bsg
